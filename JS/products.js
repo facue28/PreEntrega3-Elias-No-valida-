c14482791b82ec722c77a508e7f7fe78d0885fe4
Facundo Elias <facundo.elias10@gmail.com>
2023-08-14 20:54:44 -0300

    FiltosAgregados

--- JS/products.js
+++ JS/products.js
@@ -4,6 +4,9 @@ const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const contenedor = document.querySelector("#container-productos");
 
+// Busqueda 
+
+
 productosDisponibles.forEach(producto => {
     const div = document.createElement("div");
     div.classList.add("card", "m-3")
@@ -18,12 +21,44 @@ productosDisponibles.forEach(producto => {
 
     contenedor.appendChild(div)
 });
-const buttonCompra = document.querySelectorAll(".buttonCompra");
+
+
+
+
+const buttonFilter = document.querySelectorAll(".buttonFilter");
+    buttonFilter.forEach((button) => {
+        button.addEventListener("click", (event) =>{
+            const catProduct = event.target.getAttribute("id");
+            const catFind = productosDisponibles.filter((producto) => producto.categoria === catProduct);
+            ///console.log(catFind)
+            ///Limpiar contenedor
+            contenedor.innerHTML = ``;
+            catFind.forEach((producto) =>{
+            const div = document.createElement("div");
+            div.classList.add("card", "m-3")
+            div.style.width = "18rem"
+        
+            div.innerHTML = `<img src="..." class="card-img-top" alt="...">
+            <div class="card-body" id = ${producto.id}>
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text">Precio: ${producto.precio}.</p>
+                <a href="#" class="btn btn-primary buttonCompra" id = ${producto.id}>Agregar al carrito</a>
+            </div>`
+        
+            contenedor.appendChild(div)
+        });
+            
+        })
+        
+        
+    })
+
+
+    const buttonCompra = document.querySelectorAll(".buttonCompra");
     buttonCompra.forEach((button) => {
         button.addEventListener("click", (event) => {
             const productId = parseInt(event.target.getAttribute("id"));
-            const productFind = productosDisponibles.find((producto) => producto.id === productId
-            );
+            const productFind = productosDisponibles.find((producto) => producto.id === productId);
             if (productFind) {
                 carrito.push(productFind);
                 console.log(`Producto: ${productFind.nombre}. Agregado al carrito`);
@@ -32,5 +67,23 @@ const buttonCompra = document.querySelectorAll(".buttonCompra");
         
     });
 })
+
+
+
+// const categoria = document.querySelectorAll(".buttonCompra");
+//     buttonCompra.forEach((button) => {
+//         button.addEventListener("click", (event) => {
+//             const productId = parseInt(event.target.getAttribute("id"));
+//             const productFind = productosDisponibles.find((producto) => producto.id === productId
+//             );
+//             if (productFind) {
+//                 carrito.push(productFind);
+//                 console.log(`Producto: ${productFind.nombre}. Agregado al carrito`);
+//                 localStorage.setItem("carrito", JSON.stringify(carrito));
+//             }
+        
+//     });
+// })
+
 
 
